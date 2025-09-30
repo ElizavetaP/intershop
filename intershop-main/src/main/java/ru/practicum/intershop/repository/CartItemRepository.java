b3a96986716ae53e85bff584953bff7657906773
elizavetavolkova <lizap@bk.ru>
2025-09-30 13:17:09 +0200

    fix CartItemRepository and OrderRepository (find by username)

--- intershop-main/src/main/java/ru/practicum/intershop/repository/CartItemRepository.java
+++ intershop-main/src/main/java/ru/practicum/intershop/repository/CartItemRepository.java
@@ -11,19 +11,20 @@ import ru.practicum.intershop.model.CartItem;
 public interface CartItemRepository extends R2dbcRepository<CartItem, Long> {
 
     @Modifying
-    @Query("UPDATE cart_item SET quantity = quantity + 1 WHERE id = :id AND orders_id IS NULL")
-    Mono<Integer> incrementCountById(@Param("id") Long id);
+    @Query("UPDATE cart_item SET quantity = quantity + 1 WHERE id = :id AND orders_id IS NULL AND username = :username")
+    Mono<Integer> incrementCountById(@Param("id") Long id, @Param("username") String username);
 
     @Modifying
-    @Query("UPDATE cart_item SET quantity = quantity - 1 WHERE id = :id AND orders_id IS NULL")
-    Mono<Integer> decrementCountById(@Param("id") Long id);
+    @Query("UPDATE cart_item SET quantity = quantity - 1 WHERE id = :id AND orders_id IS NULL AND username = :username")
+    Mono<Integer> decrementCountById(@Param("id") Long id, @Param("username") String username);
 
-    // Метод для получения одного CartItem по itemId, где orders_id == null (заказ еще не создан)
-    @Query("SELECT * FROM cart_item WHERE item_id = :itemId AND orders_id IS NULL")
-    Mono<CartItem> getCartItemByItemIdAndOrderIsNull(@Param("itemId") Long itemId);
+    // Метод для получения одного CartItem по itemId для конкретного пользователя (корзина)
+    @Query("SELECT * FROM cart_item WHERE item_id = :itemId AND orders_id IS NULL AND username = :username")
+    Mono<CartItem> getCartItemByItemIdAndOrderIsNull(@Param("itemId") Long itemId, @Param("username") String username);
 
-    @Query("SELECT * FROM cart_item WHERE orders_id IS NULL")
-    Flux<CartItem> getAllNewCartItem();
+    // Получить все товары в корзине конкретного пользователя
+    @Query("SELECT * FROM cart_item WHERE orders_id IS NULL AND username = :username")
+    Flux<CartItem> getAllNewCartItem(@Param("username") String username);
 
     // Дополнительные методы для работы с заказами
     @Query("SELECT * FROM cart_item WHERE orders_id = :orderId")
--- intershop-main/src/main/java/ru/practicum/intershop/repository/OrderRepository.java
+++ intershop-main/src/main/java/ru/practicum/intershop/repository/OrderRepository.java
@@ -1,7 +1,15 @@
 package ru.practicum.intershop.repository;
 
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import ru.practicum.intershop.model.Order;
 
 public interface OrderRepository extends R2dbcRepository<Order, Long> {
+    
+    // Найти все заказы конкретного пользователя
+    Flux<Order> findByUsername(String username);
+    
+    // Найти заказ по ID и пользователю
+    Mono<Order> findByIdAndUsername(Long id, String username);
 }
